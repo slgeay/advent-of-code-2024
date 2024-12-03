68e2838226a9524f333edc84c032a57bea7ebc23
slgeay <avomailbox-github@yahoo.fr>
2024-12-03 12:21:58 +0100

    Improve Day02 to follow clippy advice

--- src/bin/02.rs
+++ src/bin/02.rs
@@ -1,35 +1,53 @@
 advent_of_code::solution!(2);
 
 pub fn part_one(input: &str) -> Option<u32> {
-    Some(input.lines().filter_map(|line| {
-        let levels = line.split_whitespace().map(|level| level.parse::<i32>().unwrap()).collect::<Vec<i32>>();
-        let direction = if levels[1] - levels[0] > 0 { 1 } else { -1 };
-        for i in 1..levels.len() {
-            let diff = (levels[i] - levels[i - 1]) * direction;
-            if diff < 1 || diff > 3 {
-                return None;
-            }
-        }
-        Some(())
-    }).count() as u32)
+    Some(
+        input
+            .lines()
+            .filter_map(|line| {
+                let levels = line
+                    .split_whitespace()
+                    .map(|level| level.parse::<i32>().unwrap())
+                    .collect::<Vec<i32>>();
+                let direction = if levels[1] - levels[0] > 0 { 1 } else { -1 };
+                for i in 1..levels.len() {
+                    let diff = (levels[i] - levels[i - 1]) * direction;
+                    if !(1..4).contains(&diff) {
+                        return None;
+                    }
+                }
+                Some(())
+            })
+            .count() as u32,
+    )
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
-    Some(input.lines().filter_map(|line| {
-        let levels = line.split_whitespace().map(|level| level.parse::<i32>().unwrap()).collect::<Vec<i32>>();
-        (0..levels.len()).any(|i| {
-            let mut levels = levels.clone();
-            levels.remove(i);
-            let direction = if levels[1] - levels[0] > 0 { 1 } else { -1 };
-            for i in 1..levels.len() {
-                let diff = (levels[i] - levels[i - 1]) * direction;
-                if diff < 1 || diff > 3 {
-                    return false;
-                }
-            }
-            true
-        }).then_some(())
-    }).count() as u32)
+    Some(
+        input
+            .lines()
+            .filter_map(|line| {
+                let levels = line
+                    .split_whitespace()
+                    .map(|level| level.parse::<i32>().unwrap())
+                    .collect::<Vec<i32>>();
+                (0..levels.len())
+                    .any(|i| {
+                        let mut levels = levels.clone();
+                        levels.remove(i);
+                        let direction = if levels[1] - levels[0] > 0 { 1 } else { -1 };
+                        for i in 1..levels.len() {
+                            let diff = (levels[i] - levels[i - 1]) * direction;
+                            if !(1..4).contains(&diff) {
+                                return false;
+                            }
+                        }
+                        true
+                    })
+                    .then_some(())
+            })
+            .count() as u32,
+    )
 }
 
 #[cfg(test)]
