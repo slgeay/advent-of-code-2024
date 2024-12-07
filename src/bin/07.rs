1539129770622c3bad245fcbb1f1078baaee0c6d
slgeay <avomailbox-github@yahoo.fr>
2024-12-07 15:08:29 +0100

    Format

--- src/bin/07.rs
+++ src/bin/07.rs
@@ -4,59 +4,69 @@ advent_of_code::solution!(7);
 
 pub fn part_one(i: &str) -> Option<u64> {
     let lines = i.lines().collect::<Vec<_>>();
-    Some(lines.iter().map(|l| {
-            let (total, expr) = l.split_once(": ").unwrap();
-            let total = total.parse().unwrap();
-            let numbers = expr.split(" ").map(|e| e.parse().unwrap()).collect();
-            let mut queue: VecDeque<VecDeque<u64>> = VecDeque::new();
-            queue.push_back(numbers);
-            while queue.len() > 0 {
-                let mut numbers = queue.pop_front().unwrap();
-                if numbers.len() == 1 {
-                    if numbers.pop_front().unwrap() == total {
-                        return total;
+    Some(
+        lines
+            .iter()
+            .map(|l| {
+                let (total, expr) = l.split_once(": ").unwrap();
+                let total = total.parse().unwrap();
+                let numbers = expr.split(" ").map(|e| e.parse().unwrap()).collect();
+                let mut queue: VecDeque<VecDeque<u64>> = VecDeque::new();
+                queue.push_back(numbers);
+                while !queue.is_empty() {
+                    let mut numbers = queue.pop_front().unwrap();
+                    if numbers.len() == 1 {
+                        if numbers.pop_front().unwrap() == total {
+                            return total;
+                        }
+                    } else {
+                        let (a, b) = (numbers.pop_front().unwrap(), numbers.pop_front().unwrap());
+                        let mut mul_numbers = numbers.clone();
+                        mul_numbers.push_front(a * b);
+                        queue.push_front(mul_numbers);
+                        numbers.push_front(a + b);
+                        queue.push_front(numbers);
                     }
-                } else {
-                    let (a, b) = (numbers.pop_front().unwrap(), numbers.pop_front().unwrap());
-                    let mut mul_numbers = numbers.clone();
-                    mul_numbers.push_front(a * b);
-                    queue.push_front(mul_numbers);
-                    numbers.push_front(a + b);
-                    queue.push_front(numbers);
                 }
-            }
-            0
-        }).sum())
+                0
+            })
+            .sum(),
+    )
 }
 
 pub fn part_two(i: &str) -> Option<u64> {
     let lines = i.lines().collect::<Vec<_>>();
-    Some(lines.iter().map(|l| {
-            let (total, expr) = l.split_once(": ").unwrap();
-            let total = total.parse().unwrap();
-            let numbers = expr.split(" ").map(|e| e.parse().unwrap()).collect();
-            let mut queue: VecDeque<VecDeque<u64>> = VecDeque::new();
-            queue.push_back(numbers);
-            while queue.len() > 0 {
-                let mut numbers = queue.pop_front().unwrap();
-                if numbers.len() == 1 {
-                    if numbers.pop_front().unwrap() == total {
-                        return total;
+    Some(
+        lines
+            .iter()
+            .map(|l| {
+                let (total, expr) = l.split_once(": ").unwrap();
+                let total = total.parse().unwrap();
+                let numbers = expr.split(" ").map(|e| e.parse().unwrap()).collect();
+                let mut queue: VecDeque<VecDeque<u64>> = VecDeque::new();
+                queue.push_back(numbers);
+                while !queue.is_empty() {
+                    let mut numbers = queue.pop_front().unwrap();
+                    if numbers.len() == 1 {
+                        if numbers.pop_front().unwrap() == total {
+                            return total;
+                        }
+                    } else {
+                        let (a, b) = (numbers.pop_front().unwrap(), numbers.pop_front().unwrap());
+                        let mut mul_numbers = numbers.clone();
+                        let mut cat_numbers = numbers.clone();
+                        cat_numbers.push_front((a.to_string() + &b.to_string()).parse().unwrap());
+                        queue.push_front(cat_numbers);
+                        mul_numbers.push_front(a * b);
+                        queue.push_front(mul_numbers);
+                        numbers.push_front(a + b);
+                        queue.push_front(numbers);
                     }
-                } else {
-                    let (a, b) = (numbers.pop_front().unwrap(), numbers.pop_front().unwrap());
-                    let mut mul_numbers = numbers.clone();
-                    let mut cat_numbers = numbers.clone();
-                    cat_numbers.push_front((a.to_string() + &b.to_string()).parse().unwrap());
-                    queue.push_front(cat_numbers);
-                    mul_numbers.push_front(a * b);
-                    queue.push_front(mul_numbers);
-                    numbers.push_front(a + b);
-                    queue.push_front(numbers);
                 }
-            }
-            0
-        }).sum())
+                0
+            })
+            .sum(),
+    )
 }
 
 #[cfg(test)]
